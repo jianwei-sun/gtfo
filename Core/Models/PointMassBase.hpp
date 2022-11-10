b8cea4d6f87015c0bfa88103cf1cef85e191a292
Control Computer <FullBodyHapticExo@gmail.com>
2022-11-09 18:49:27 -0800

    Added basic boundry logic.  No actual bound class yet

--- Core/Models/PointMassBase.hpp
+++ Core/Models/PointMassBase.hpp
@@ -11,79 +11,98 @@
 // Third-party dependencies
 #include <Eigen/Dense>
 
-namespace gtfo{
+namespace gtfo
+{
 
-template<typename Scalar = double>
-struct ParametersBase{
-    Scalar dt;
+    template <typename Scalar = double>
+    struct ParametersBase
+    {
+        Scalar dt;
 
-    ParametersBase(const Scalar& dt) : dt(dt){
-        assert(dt > 0.0);
-    }
-};
+        ParametersBase(const Scalar &dt) : dt(dt)
+        {
+            assert(dt > 0.0);
+        }
+    };
 
-template<unsigned int Dimensions, typename Parameters, typename Scalar = double>
-class PointMassBase{
-public:
-    static_assert(Dimensions > 0, "Dimensions must be at least 1.");
-    static_assert(std::is_floating_point_v<Scalar>, "Template argument Scalar must be a floating-point type.");
+    template <unsigned int Dimensions, typename Parameters, typename Scalar = double>
+    class PointMassBase
+    {
+    public:
+        static_assert(Dimensions > 0, "Dimensions must be at least 1.");
+        static_assert(std::is_floating_point_v<Scalar>, "Template argument Scalar must be a floating-point type.");
 
-    using VectorN = Eigen::Matrix<Scalar, Dimensions, 1>;
-    using Vector2 = Eigen::Matrix<Scalar, 2, 1>;
-    using Matrix2 = Eigen::Matrix<Scalar, 2, 2>;
-    using PossibleScalar = std::conditional_t<Dimensions == 1, Scalar, VectorN>;
+        using VectorN = Eigen::Matrix<Scalar, Dimensions, 1>;
+        using Vector2 = Eigen::Matrix<Scalar, 2, 1>;
+        using Matrix2 = Eigen::Matrix<Scalar, 2, 2>;
 
-    PointMassBase()
-        :   A_continuous_(Matrix2::Zero()),
-            B_continuous_(Vector2::Zero()),
-            A_discrete_(Matrix2::Zero()),
-            B_discrete_(Vector2::Zero()),
-            position_(VectorN::Zero()),
-            velocity_(VectorN::Zero()),
-            acceleration_(VectorN::Zero()){}
+        PointMassBase()
+            : A_discrete_(Matrix2::Zero()),
+              B_discrete_(Vector2::Zero()),
+              position_(VectorN::Zero()),
+              velocity_(VectorN::Zero()),
+              acceleration_(VectorN::Zero())
+        {
+        }
 
-    virtual void SetParameters(const Parameters& parameters) = 0;
+        virtual void SetParameters(const Parameters &parameters) = 0;
 
-    // Propagate the dynamics forward by one time-step
-    virtual void Step(const PossibleScalar& input){
-        const Eigen::Matrix<Scalar, 2, Dimensions> state = (Eigen::Matrix<Scalar, 2, Dimensions>() << position_.transpose(), velocity_.transpose()).finished();
-        const Eigen::Matrix<Scalar, 2, Dimensions> new_state = A_discrete_ * state + (input * B_discrete_.transpose()).transpose();
-        position_ = new_state.row(0);
-        velocity_ = new_state.row(1);
-        acceleration_ = A_continuous_.row(1) * state + (input * B_continuous_.row(1).transpose()).transpose();
-    }
+        void SetPositionBound(const Bound<Scalar, Dimensions> &position_bound)
+        {
+            position_bound_ = position_bound;
+        }
 
-    [[nodiscard]] inline const PossibleScalar& GetPosition() const {
-        if constexpr(Dimensions == 1)
-            return position_.value();
-        else
-            return position_; 
-    }
+        void SetVelocityBound(const Bound<Scalar, Dimensions> &velocity_bound)
+        {
+            velocity_bound_ = velocity_bound;
+        }
 
-    [[nodiscard]] inline const PossibleScalar& GetVelocity() const {
-        if constexpr(Dimensions == 1)
-            return velocity_.value();
-        else
+        void SetUserInputBound(const Bound<Scalar, Dimensions> &user_input_bound)
+        {
+            user_input_bound_ = user_input_bound;
+        }
+
+        // Propagate the dynamics forward by one time-step
+        virtual void Step(const VectorN &user_input, const VectorN &environment_input = VectorN::Zero())
+        {
+            const VectorN total_input = user_input_bound_(user_input) + environment_input;
+            const Eigen::Matrix<Scalar, 2, Dimensions> state = (Eigen::Matrix<Scalar, 2, Dimensions>() << position_.transpose(), velocity_.transpose()).finished();
+            const Eigen::Matrix<Scalar, 2, Dimensions> new_state = A_discrete_ * state + (total_input * B_discrete_.transpose()).transpose();
+
+            position_ = position_bound_(new_state.row(0));
+            velocity_ = velocity_bound_(new_state.row(1));
+            acceleration_ = (velocity_ - state.row(1)) / parameters_.dt;
+        }
+
+        [[nodiscard]] inline const VectorN &GetPosition() const
+        {
+            return position_;
+        }
+
+        [[nodiscard]] inline const VectorN &GetVelocity() const
+        {
             return velocity_;
-    }
+        }
 
-    [[nodiscard]] inline const PossibleScalar& GetAcceleration() const {
-        if constexpr(Dimensions == 1)
-            return acceleration_.value();
-        else
+        [[nodiscard]] inline const VectorN &GetAcceleration() const
+        {
             return acceleration_;
-    }
+        }
 
-protected:
-    Matrix2 A_continuous_;
-    Vector2 B_continuous_;
+    protected:
+        Parameters parameters_;
 
-    Matrix2 A_discrete_;
-    Vector2 B_discrete_;
+        Matrix2 A_discrete_;
+        Vector2 B_discrete_;
 
-    VectorN position_;
-    VectorN velocity_;
-    VectorN acceleration_;
-};
+        VectorN position_;
+        VectorN velocity_;
+        VectorN acceleration_;
 
-}   // namespace gtfo
+    private:
+        Bound<Scalar, Dimensions> position_bound_;
+        Bound<Scalar, Dimensions> velocity_bound_;
+        Bound<Scalar, Dimensions> user_input_bound_;
+    };
+
+} // namespace gtfo
