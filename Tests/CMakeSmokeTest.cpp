c9ef5ba69daaf0fa91cba67f29af1718cbaa4f0e
Jianwei Sun <jian58sun@gmail.com>
2023-01-18 15:59:12 -0600

    Added mujoco to cmake build environment

--- Tests/CMakeSmokeTest.cpp
+++ Tests/CMakeSmokeTest.cpp
@@ -1,8 +1,21 @@
 #include <gtest/gtest.h>
+#include <Eigen/Dense>
+#include <mujoco/mujoco.h>
 
 // Tests that the environment has been set up correctly 
 TEST(CMakeSmokeTest, BasicAssertions) {
 
   // Expect two strings not to be equal
   EXPECT_STRNE("hello", "world");
+}
+
+// Verifies version information for Eigen
+TEST(CMakeSmokeTest, EigenVersion){
+  EXPECT_TRUE(EIGEN_VERSION_AT_LEAST(3, 4, 0));
+}
+
+// Verifies that MuJoCo is compiled and linked correctly
+TEST(CMakeSmokeTest, MuJoCoVersion){
+  EXPECT_TRUE(mjVERSION_HEADER == mj_version());
+  EXPECT_EQ(mjVERSION_HEADER, 231);
 }
