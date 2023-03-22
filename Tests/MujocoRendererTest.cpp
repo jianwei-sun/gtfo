5af0de54df3c472133c35305b86a2c4ed312e2b1
Ishita Ghosh <ishitagh16@gmail.com>
2023-03-22 15:37:06 -0700

    Added mouse and keyboard callbacks

--- Tests/MujocoRendererTest.cpp
+++ Tests/MujocoRendererTest.cpp
@@ -6,31 +6,17 @@
 
 TEST(MujocoRendererTest, SmokeTest)
 {
-    std::cout << "It loaded the test :)" << std::endl;
     using VectorN = gtfo::MujocoModel<7>::VectorN;
     gtfo::MujocoModel<7> mujoco_wrapper("arms.xml", 0.001);
-    std::cout << "creating the model" << std::endl;
-
     mujoco_wrapper.Step(VectorN::Constant(0.1));
 
     gtfo::MujocoRenderer mujoco_renderer(mujoco_wrapper);
 
-    std::cout << "started the renderer" << std::endl;
-
-
     for(unsigned i = 0; i < 1000; ++i){
         mujoco_wrapper.Step(VectorN::Constant(0.1));
-        //debug with 1 thread first
-        // mujoco_renderer.render();
-
         std::this_thread::sleep_for(std::chrono::milliseconds(15));
-
     }
-    std::cout << "got past step" << std::endl;
-
     mujoco_renderer.set_should_render(false);
 
-    EXPECT_TRUE(false);
-    
-
+    EXPECT_TRUE(false); //
 }
--- Wrapper/MujocoRenderer.hpp
+++ Wrapper/MujocoRenderer.hpp
@@ -27,14 +27,14 @@ template<unsigned int Dimensions>
 class MujocoRenderer{
 public:
 void render() {
-    // update scene and render
     // wrapper_.Get_Lock().lock();
+
     // get framebuffer viewport
     viewport_ = {0, 0, 0, 0};
     glfwGetFramebufferSize(window_, &viewport_.width, &viewport_.height);
     mjv_updateScene(wrapper_.Get_Model(), wrapper_.Get_Data(), &opt_, NULL, &cam_, mjCAT_ALL, &scn_);
-    // if(&con_)
     mjr_render(viewport_, &scn_, &con_);
+    
     // wrapper_.Get_Lock().unlock();
 
     // swap OpenGL buffers (blocking call due to v-sync)
@@ -42,110 +42,148 @@ void render() {
 
     // process pending GUI events, call GLFW callbacks
     glfwPollEvents();
-    
-    //set mouse hooks so it can be interactive
 }
 
-void keep_rendering() {
+// keyboard callback
+void keyboard(GLFWwindow* window, int key, int scancode, int act, int mods)
+{
+    // backspace: reset simulation
+    if( act==GLFW_PRESS && key==GLFW_KEY_BACKSPACE )
+    {
+        mj_resetData(wrapper_.Get_Model(), wrapper_.Get_Data());
+        mj_forward(wrapper_.Get_Model(), wrapper_.Get_Data());
+    }
+}
+
+// mouse move callback
+void mouse_move(GLFWwindow* window, double xpos, double ypos)
+{
+    // no buttons down: nothing to do
+    if( !button_left && !button_middle && !button_right )
+        return;
+
+    // compute mouse displacement, save
+    double dx = xpos - lastx;
+    double dy = ypos - lasty;
+    lastx = xpos;
+    lasty = ypos;
+
+    // get current window size
+    int width, height;
+    glfwGetWindowSize(window, &width, &height);
+
+    // get shift key state
+    bool mod_shift = (glfwGetKey(window, GLFW_KEY_LEFT_SHIFT)==GLFW_PRESS ||
+                      glfwGetKey(window, GLFW_KEY_RIGHT_SHIFT)==GLFW_PRESS);
+
+    // determine action based on mouse button
+    mjtMouse action;
+    if( button_right )
+        action = mod_shift ? mjMOUSE_MOVE_H : mjMOUSE_MOVE_V;
+    else if( button_left )
+        action = mod_shift ? mjMOUSE_ROTATE_H : mjMOUSE_ROTATE_V;
+    else
+        action = mjMOUSE_ZOOM;
+
+    // move camera
+    mjv_moveCamera(wrapper_.Get_Model(), action, dx/height, dy/height, &scn_, &cam_);
+}
+
+// mouse button callback
+void mouse_button(GLFWwindow* window, int button, int act, int mods)
+{
+    // update button state
+    button_left =   (glfwGetMouseButton(window, GLFW_MOUSE_BUTTON_LEFT)==GLFW_PRESS);
+    button_middle = (glfwGetMouseButton(window, GLFW_MOUSE_BUTTON_MIDDLE)==GLFW_PRESS);
+    button_right =  (glfwGetMouseButton(window, GLFW_MOUSE_BUTTON_RIGHT)==GLFW_PRESS);
+
+    // update mouse position
+    glfwGetCursorPos(window, &lastx, &lasty);
+}
+
+// scroll callback
+void scroll(GLFWwindow* window, double xoffset, double yoffset)
+{
+    // emulate vertical mouse motion = 5% of window height
+    mjv_moveCamera(wrapper_.Get_Model(), mjMOUSE_ZOOM, 0, -0.05*yoffset, &scn_, &cam_);
+}
+
+void keep_rendering(void) {
+    if (!glfwInit()) {
+            mju_error("Could not initialize GLFW");
+    }
+
+    // #ifdef __APPLE__
+    /* We need to explicitly ask for a 3.3 context on OS X */
+    glfwWindowHint (GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint (GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint (GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+    glfwWindowHint (GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+    // #endif
+
+    window_ = glfwCreateWindow(800, 600, "Test", NULL, NULL);
+    glfwMakeContextCurrent(window_);
+    glfwSwapInterval(1);
+    mjv_defaultCamera(&cam_);
+    mjv_defaultOption(&opt_);
+    mjr_defaultContext(&con_);
+    mjv_defaultScene(&scn_);
+    mjv_makeScene(wrapper_.Get_Model(), &scn_, 1000);                     // space for 1000 objects
+    mjr_makeContext(wrapper_.Get_Model(), &con_, mjFONTSCALE_100);        // model-specific context
+    
+    // install GLFW mouse and keyboard callbacks
+    glfwSetKeyCallback(window_, keyboard);
+    glfwSetCursorPosCallback(window_, mouse_move);
+    glfwSetMouseButtonCallback(window_, mouse_button);
+    glfwSetScrollCallback(window_, scroll);
+
     while(should_render_) {
         render();
-        // FIX!!!
-        // typedef std::chrono::duration<int, std::ratio<1, 60>> frame_duration;
-        // frame_end_time += std::chrono::duration_cast<std::chrono::seconds>(frame_duration(1));
-        // std::this_thread::sleep_until(frame_end_time);
-
-        // CAN USE SOMETHING LIKE THIS FOR THE WAITING INSTEAD??
-        // advance interactive simulation for 1/60 sec
-        //  Assuming MuJoCo can simulate faster than real-time, which it usually can,
-        //  this loop will finish on time for the next frame to be rendered at 60 fps.
-        //  Otherwise add a cpu timer and exit this loop when it is time to render.
-        // mjtNum simstart = d->time;
-        // while( d->time - simstart < 1.0/60.0 )
-        //     mj_step(m, d);
-
         std::this_thread::sleep_for(std::chrono::milliseconds(100));
     }
-}
 
-void rendering_loop(int loop_count) {
-    //rendering_indefinitely = true: keep running
-    for (int i = 0; i < loop_count; i++) {
-        render();
-        std::this_thread::sleep_for(std::chrono::milliseconds(200));
-    }
-}
-//MAKE ANOTHER FN THAT WOULD SET IT TO FALSE AND STOP THE THREAD
-
-MujocoRenderer(MujocoModel<Dimensions>& wrap) //removed const
-        :   wrapper_(wrap),
-            rendering_thread_{}//,
-            // scn_(nullptr),
-            // con_(nullptr)
-    {
-        std::cout << "Actually gets into the constructor" << std::endl;
-        //set up window
-
-        if (!glfwInit()) {
-            mju_error("Could not initialize GLFW");
-        }
-
-        window_ = glfwCreateWindow(800, 600, "Test", NULL, NULL);
-        glfwMakeContextCurrent(window_);
-        glfwSwapInterval(1);
-        mjv_defaultCamera(&cam_);
-        // mjv_defaultPerturb(&pert_);
-        mjv_defaultOption(&opt_);
-        mjr_defaultContext(&con_);
-        mjv_defaultScene(&scn_);
-        // const mjModel* model_ptr = wrapper_.Get_Model();
-        // if(model_ptr)
-        //     std::cout << "Model pointer is not null" << std::endl;
-        mjv_makeScene(wrapper_.Get_Model(), &scn_, 1000);                     // space for 1000 objects
-        std::cout << "Works till here" << std::endl;
-        mjr_makeContext(wrapper_.Get_Model(), &con_, mjFONTSCALE_100);     // model-specific context
-
-        rendering_thread_ = std::thread{&MujocoRenderer::keep_rendering, this};
-
-        std::cout << "Constructed successfully before the thread stuff" << std::endl;
-
-        // rendering_thread_ = std::thread([this/*, &wrapper_ = wrapper_*/](){
-        //     this->keep_rendering();
-        // });
-
-        std::cout << "Constructed successfully" << std::endl;
-
-    }
-
-    void set_should_render(bool status) {
-        should_render_ = status;
-    }
-
-~MujocoRenderer(){
-    // close GLFW, free visualization storage
-    glfwTerminate();
+    // free visualization storage, close GLFW
     mjv_freeScene(&scn_);
     mjr_freeContext(&con_);
-    
-    //SHOULD THIS BE STD::this_thread.join() INSTEAD??
-    // rendering_thread_.join();
+    glfwTerminate();
+}
+
+MujocoRenderer(MujocoModel<Dimensions>& wrap)
+        :   wrapper_(wrap), rendering_thread_{}
+{
+    rendering_thread_ = std::thread{&MujocoRenderer::keep_rendering, this};
+}
+
+void set_should_render(bool status) {
+    should_render_ = status;
+}
+
+~MujocoRenderer(){
+    rendering_thread_.join();
 
 }
 
 private:
     mjrRect viewport_; 
-    mjvScene scn_;                     //SHOULD THESE BE POINTERS??
-    mjrContext con_;                   // SHOULD THESE BE POINTERS??
+    mjvScene scn_;
+    mjrContext con_;
     GLFWwindow* window_;
     mjvCamera cam_;                      //abstract camera
     mjvPerturb pert_;                    // perturbation object
     mjvOption opt_;                      // visualization options
 
-     MujocoModel<Dimensions>& wrapper_; //not const because mjv_updateScene changes this
+    MujocoModel<Dimensions>& wrapper_; //not const because mjv_updateScene changes this
 
     std::thread rendering_thread_;
     
-
     bool should_render_ = true;
+
+    // mouse interaction
+    bool button_left = false;
+    bool button_middle = false;
+    bool button_right =  false;
+    double lastx = 0;
+    double lasty = 0;
 };
 
 } //namespace gtfo
