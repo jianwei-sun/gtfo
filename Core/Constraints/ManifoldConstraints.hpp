629d6d28ecd3092de18e155fddc795467beacd3c
Jianwei Sun <jian58sun@gmail.com>
2023-06-14 12:29:02 -0700

    Added setters for constraints and gain

--- Core/Constraints/ManifoldConstraints.hpp
+++ Core/Constraints/ManifoldConstraints.hpp
@@ -10,6 +10,8 @@
 
 // Third-party dependencies
 #include <Eigen/Dense>
+#include <Eigen/KroneckerProduct>
+
 
 // Project-specific
 
@@ -32,29 +34,49 @@ public:
     using ConstraintFunction = std::function<VectorK(const VectorN&)>;
     using ConstraintFunctionGradient = std::function<MatrixKN(const VectorN&)>;
     using ConstraintFunctionHessianSlice = std::function<MatrixNN(const VectorN&)>;
+    using TransversalGain = Eigen::Matrix<Scalar, ConstraintDimension, 2*ConstraintDimension>;
 
     ManifoldConstraints()
     :   constraint_function_(nullptr),
         constraint_function_gradient_(nullptr),
-        constraint_function_hessian_slices_{}
+        constraint_function_hessian_slices_{},
+        transversal_gain_(TransversalGain::Zero())
     {}
 
+    // Calculate on each iteration after constraint function and gains have been set to create the new forces
     VectorN Step(const VectorN& force, const VectorN& position, const VectorN& velocity){
         if (!constraint_function_){
             return force;
         }
+
     }
 
+    // Set constraint function, and first and second partial derivative. Because second partial is a tensor, we enter hessian slices instead
     void SetConstraintFunction(const ConstraintFunction& constraint_function, const ConstraintFunctionGradient& constraint_function_gradient, const std::array<ConstraintFunctionHessianSlice, ConstraintDimension>& constraint_function_hessian_slices){
         constraint_function_ = constraint_function;
         constraint_function_gradient_ = constraint_function_gradient;
         constraint_function_hessian_slices_ = constraint_function_hessian_slices;
     }
 
+    // Transversal gain in the general case where the whole matrix is entered. This is for when the gains are different in each constraint coordinate
+    void SetTransversalGain(const Eigen::Matrix<Scalar, ConstraintDimension, 2*ConstraintDimension>& transversal_gain){
+        assert((transversal_gain.array() >= 0).all());
+
+        transversal_gain_ = transversal_gain;
+    }
+
+    // Transversal Gain when the gains are similar for all constraint coordinates
+    void SetTransversalGain(const Eigen::Matrix<Scalar, 1, 2>& transversal_gain_i){
+        assert((transversal_gain_i.array() >= 0).all());
+
+        transversal_gain_ = Eigen::kroneckerProduct (Eigen::Matrix<Scalar, ConstraintDimension, ConstraintDimension>::Identity(), transversal_gain_i);
+    }
+
 private:
     ConstraintFunction constraint_function_;
     ConstraintFunctionGradient constraint_function_gradient_;
     std::array<ConstraintFunctionHessianSlice, ConstraintDimension> constraint_function_hessian_slices_;
+    TransversalGain transversal_gain_;
 };
 
 } // namespace gtfo
