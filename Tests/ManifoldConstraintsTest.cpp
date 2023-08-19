97910af941f5ac17f143747fe3d83908710d5791
Jianwei Sun <jian58sun@gmail.com>
2023-08-18 17:00:32 -0700

    Replaced with constexpr and rebased

--- Tests/ManifoldConstraintsTest.cpp
+++ Tests/ManifoldConstraintsTest.cpp
@@ -191,7 +191,6 @@ TEST(ManifoldConstraintsTest, XYSurfaceConstraint)
     // Check if x and y match system with no constraint (constraint should only affect z)
     EXPECT_TRUE(gtfo::IsEqual(system.GetPosition().segment<2>(0), system_no_constraint.GetPosition().segment<2>(0)));
 
-<<<<<<< HEAD
 }
 
 TEST(ManifoldConstraintsTest, CircularPathConstraint)
@@ -289,7 +288,7 @@ TEST(ManifoldConstraintsTest, EllipticalPathConstraint)
     constexpr double b = 2;
     constexpr double x0 = 0;
     constexpr double y0 = 0;
-    const unsigned trials = 1000;
+    constexpr unsigned trials = 1000;
 
     // Vector Types
     using VectorN = Eigen::Matrix<double, state_dimension, 1>;
@@ -305,9 +304,9 @@ TEST(ManifoldConstraintsTest, EllipticalPathConstraint)
 
     const double wn = 0.5;
     const double zeta = 1;
-    const Eigen::RowVector2d transversal_gain(wn*wn, 2*zeta*wn);
-    const Eigen::Vector3d initial_position(0, 0, 1); // start at z=1, x=0.5, should move to z=0 x=1
-    const VectorN initial_velocity = VectorN::Zero();
+    static const Eigen::RowVector2d transversal_gain(wn*wn, 2*zeta*wn);
+    static const Eigen::Vector3d initial_position(0, 0, 1); // start at z=1, x=0.5, should move to z=0 x=1
+    static const VectorN initial_velocity = VectorN::Zero();
 
     // Define System
     gtfo::PointMassSecondOrder<state_dimension> system((gtfo::SecondOrderParameters<double>(cycle_time_step, mass, damping)));
@@ -363,100 +362,4 @@ TEST(ManifoldConstraintsTest, EllipticalPathConstraint)
     EXPECT_NEAR(std::pow(system.GetPosition()[0] - x0, 2)/a/a + std::pow(system.GetPosition()[1] - y0, 2)/b/b, 1, 0.01);
     // Check if z is zero
     EXPECT_NEAR(system.GetPosition()[2], 0.0, 0.01);
-    EXPECT_TRUE((lower - 0.1) <= system.GetPosition().value() && system.GetPosition().value() <= (upper + 0.1));
-}
-
-TEST(ManifoldConstraintsTest, BarrierConstraint2D)
-{
-    // Vector Types
-    using VectorN = Eigen::Matrix<double, 2, 1>;
-    using VectorK = Eigen::Matrix<double, 2, 1>;
-    using MatrixKN = Eigen::Matrix<double, 2, 2>;
-    using MatrixNN = Eigen::Matrix<double, 2, 2>;
-    using MatrixK2K = Eigen::Matrix<double, 2, 4>;
-
-    // Define System
-    const double mass = 1.0;
-    const double damping = 1.0;  
-    const double cycle_time_step = 0.01;
-    gtfo::PointMassSecondOrder<2> system((gtfo::SecondOrderParameters<double>(cycle_time_step, mass, damping)));
-    
-    // Define constraint surface
-    gtfo::ManifoldConstraints<2, 2> manifold_constraints;
-
-    // Define boundaries and constants
-    const VectorK lower(-1.0, -0.5);
-    const VectorK upper(0.5, 1.0);
-    const double steepness = 10.0;
-
-    // h(x)
-    const std::function<VectorK(const VectorN&)> constraint_function = [&](const VectorN& position){
-        return (position.array() < lower.array()).select(steepness * (lower - position).array().pow(3), 
-            (position.array() > upper.array()).select(steepness * (position - upper).array().pow(3),
-                VectorK::Zero()));
-    };
-
-    // h'(x)
-    const std::function<MatrixKN(const VectorN&)> constraint_function_gradient = [&](const VectorN& position){
-        return MatrixKN(VectorN(
-            (position.array() < lower.array()).select(-3.0 * steepness * (lower - position).array().pow(2),
-            (position.array() > upper.array()).select(3.0 * steepness * (position - upper).array().pow(2),
-                VectorK::Zero()))).asDiagonal());
-    };
-
-    // h''(x)
-    const std::function<MatrixNN(const VectorN&, const unsigned&)> constraint_function_hessian_slice = [&](const VectorN& position, const unsigned& index){
-        const double& x = position[index];
-        const double& l = lower[index];
-        const double& u = upper[index];
-        const double d2h = 
-            x < l ? 
-                6.0 * steepness * (l - x) : 
-            x > u ? 
-                6.0 * steepness * (x - u) : 
-                0.0;
-        if(index == 0){
-            return (MatrixNN() << d2h, 0.0, 0.0, 0.0).finished();
-        } else{
-            return (MatrixNN() << 0.0, 0.0, 0.0, d2h).finished();
-        }
-    };
-    const std::array<std::function<MatrixNN(const VectorN&)>, 2> constraint_function_hessian_slices{
-        std::bind(constraint_function_hessian_slice, std::placeholders::_1, 0),
-        std::bind(constraint_function_hessian_slice, std::placeholders::_1, 1),
-    };
-
-    // Set the constraint functions
-    manifold_constraints.SetConstraintFunction(constraint_function, constraint_function_gradient, constraint_function_hessian_slices);
-
-    // Set the virtual dynamics
-    const auto f_bottom_half = [mass, damping](const VectorN& position, const VectorN& velocity){
-        return -damping / mass * velocity; // VectorN output
-    };
-    const auto g_bottom_half = [mass, damping](const VectorN& position, const VectorN& velocity){
-        return 1/mass * MatrixNN::Identity(); // MatrixNN output
-    };
-    manifold_constraints.SetSecondOrderDynamics(f_bottom_half, g_bottom_half);
-
-    // Set the transversal gain to be the same in both dimensions
-    const double wn = 5.0;
-    const double zeta = 1.0;
-    manifold_constraints.SetTransversalGain(Eigen::RowVector2d(wn * wn, 2.0 * zeta * wn));
-
-    // Associate the manifold constraints with the dynamics
-    system.SetForcePremodifier([&](const VectorN& force, const gtfo::DynamicsBase<2>& system){
-       const VectorN modified_force = manifold_constraints.Step(force, system.GetPosition(), system.GetVelocity());
-       std::cout << modified_force.transpose() << ", ";
-       return modified_force;
-    });
-
-    // Step the system
-    std::cout << "Force, Position, Velocity" << std::endl;
-    for(unsigned i = 0; i < 500; ++i){
-        system.Step(VectorN(2.0, -1.0));
-        std::cout << system.GetPosition().transpose() << ", " << system.GetVelocity().transpose() << std::endl;
-    }
-
-    // Verify the state is in the constraint manifold to some mild tolerance 
-    EXPECT_TRUE(((lower.array() - 0.1) < system.GetPosition().array()).all() && (system.GetPosition().array() < (upper.array() + 0.1)).all());
 }
