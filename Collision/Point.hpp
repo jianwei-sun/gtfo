39054fbf3761d223c0c6f6c87e1fcedf23216bf6
GuanyuChen98 <chenguanyublithe@gmail.com>
2024-10-18 18:04:18 -0700

    find the minimum distance

--- Collision/Point.hpp
+++ Collision/Point.hpp
@@ -18,8 +18,9 @@ class Point{
 public:
     using Vector3 = Eigen::Matrix<Scalar, 3, 1>;
 
-    Point(const Vector3& point_of_interest)
-        :   point_of_interest_(point_of_interest)
+    Point(const Vector3& point_of_interest, const Vector3& point_on_curve)
+        :   point_of_interest_(point_of_interest),
+            point_on_curve_(point_on_curve)
     {
 
     }
@@ -28,13 +29,31 @@ public:
         return point_of_interest_;
     }
 
+    Vector3 PointOnCurve(void) const{
+        return point_on_curve_;
+    }
 
-    Vector3 MinDistanceVectorTo(const VirtualTunnel& other) const{
-        
+    Scalar Length(void) const{
+        return (point_on_curve_ - point_of_interest_).norm();
+    }
+
+    Point MinDistanceVectorTo(const Eigen::MatrixXd other) const{
+        double min_dist = std::numeric_limits<double>::max();
+        int index = -1;
+        for (int i = 0; i < other.rows(); ++i) {
+            double dist = (other.row(i) - point_of_interest_.transpose()).norm();
+            if (dist < min_dist) {
+                min_dist = dist;
+                index = i;
+            }
+        }
+        point_on_curve_ =  other.row(index).transpose();
+        return Point(point_of_interest_, point_on_curve_);
     }
 
 private:
     Vector3 point_of_interest_;
+    Vector3 point_on_curve_;
 };
 
 }   // namespace collision
--- Collision/VirtualTunnel.hpp
+++ Collision/VirtualTunnel.hpp
@@ -19,22 +19,64 @@
 namespace gtfo{
 namespace collision{
 
+struct PointCloud {
+	Eigen::MatrixXd elbow;
+	Eigen::MatrixXd wrist;
+};
+
 template<typename Scalar = double>
 class VirtualTunnel : public EntityPointTunnel<Scalar>{
 public:
 
     using Vector3 = typename EntityPointTunnel<Scalar>::Vector3;
   
-    VirtualTunnel(const std::vector<Vector3>& vertices)  // the vertices are actually dots along the reference trajectory
-        :   EntityPointTunnel<Scalar>(vertices, true)
+    VirtualTunnel(const std::vector<Vector3>& vertices, const TunnelParameters &tunnel_parameters)  // the vertices are actually dots along the reference trajectory
+        :   Entity<Scalar>(vertices, true)
+    {
+        // generate trajectory in joint space first
+        Eigen::Matrix<double, Eigen::Dynamic, 1> pct = Eigen::VectorXd::LinSpaced(tunnel_parameters.num_of_points, 0.0, 1.0);
+        Eigen::Matrix<double, 4, Eigen::Dynamic> theta;
+        Eigen::Matrix<double, 4, Eigen::Dynamic> start_matrix = start_configuration_.replicate(1, tunnel_parameters.num_of_points);
+        Eigen::Matrix<double, 4, Eigen::Dynamic> end_matrix = end_configuration_.replicate(1, tunnel_parameters.num_of_points);
+        Eigen::ArrayXd pct_array = pct.array();
+        Eigen::ArrayXd pct3 = pct_array * pct_array * pct_array; // pct^3
+        Eigen::ArrayXd pct4 = pct3 * pct_array; // pct^4
+        Eigen::ArrayXd pct5 = pct4 * pct_array;
+        Eigen::ArrayXd pct4_replicated = pct4.replicate(4, 1);
+        Eigen::ArrayXd pct5_replicated = pct5.replicate(4, 1);
+        Eigen::ArrayXd pct3_replicated = pct3.replicate(4, 1);  
+    
+        theta = (start_matrix.array() + 
+        (start_matrix.array() - end_matrix.array()) * 
+        (15 * pct4_replicated - 6 * pct5_replicated - 10 * pct3_replicated)).matrix();
 
-    {}
+        Eigen::RowVectorXd t1 = theta.row(0);
+        Eigen::RowVectorXd t2 = theta.row(1);
+        Eigen::RowVectorXd t3 = theta.row(2);
+        Eigen::RowVectorXd t4 = theta.row(3);
+
+        Eigen::RowVectorXd elbow_x = -tunnel_parameters.lu * (t2.array().cos() * t1.array().sin());
+        Eigen::RowVectorXd elbow_y = tunnel_parameters.lu * t2.array().sin();
+        Eigen::RowVectorXd elbow_z = -tunnel_parameters.lu * (t1.array().cos() * t2.array().cos());
+        
+        Eigen::RowVectorXd wrist_x = tunnel_parameters.lf * (t4.array().sin() * (t1.array().cos() * t3.array().sin() + t3.array().cos() * t1.array().sin() * t2.array().sin()) - t2.array().cos() * t4.array().cos() * t1.array().sin()) - tunnel_parameters.lu * t2.array().cos() * t1.array().sin();
+        Eigen::RowVectorXd wrist_y = tunnel_parameters.lf * (t4.array().cos() * t2.array().sin() + t2.array().cos() * t3.array().cos() * t4.array().sin()) + tunnel_parameters.lu * t2.array().sin();
+        Eigen::RowVectorXd wrist_z = -tunnel_parameters.lf * (t4.array().sin() * (t1.array().sin() * t3.array().sin() - t1.array().cos() * t3.array().cos() * t2.array().sin()) + t1.array().cos() * t2.array().cos() * t4.array().cos()) - tunnel_parameters.lu * t1.array().cos() * t2.array().cos();
+
+        point_cloud_.elbow.col(0) = elbow_x.transpose();
+        point_cloud_.elbow.col(1) = elbow_y.transpose();
+        point_cloud_.elbow.col(2) = elbow_z.transpose();
+
+        point_cloud_.wrist.col(0) = wrist_x.transpose();
+        point_cloud_.wrist.col(1) = wrist_y.transpose();
+        point_cloud_.wrist.col(2) = wrist_z.transpose();
+    }
+}
 
     
 private:
 
-
-};
+    PointCloud point_cloud_;
 
 }   // namespace collision
 }   // namespace gtfo
